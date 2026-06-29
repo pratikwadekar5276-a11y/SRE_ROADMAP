a452e51c6761c63dd16edec78ba2244a18f80239
Pratik Wadekar <pratikwadekar@Pratiks-MacBook-Air.local>
2026-06-29 23:34:38 +0530

    Python - 29th June 2026

---
+++ Python_Scripts/ramdom_test.py
@@ -0,0 +1,5 @@
+import random
+
+colors = ["red", "green", "blue", "yellow", "purple"]
+result = (colors[random.randint(0, len(colors) - 1)])
+print("Randomly selected color:", result)
---
+++ Python_Scripts/time_test.py
@@ -0,0 +1,10 @@
+import time
+
+a = time.time()
+print (a)
+time.sleep(20)
+
+b = time.time()
+print (b)
+print("Time taken:", b - a, "seconds")
+
